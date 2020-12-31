e7cb798dcf5897ae8e4a8976e8c941202b825ac3
Eugeniusz Mowlewicz <by.eugeniusz@gmail.com>
2020-12-31 13:32:52 +0100

    update

---
+++ RiskGroupRozkov.js
@@ -0,0 +1,14 @@
+function AnswerRGR(){
+    FamilyRelationships = 0;    // Отношения в семье
+    Aggressiveness  = 0;        // Агрессивность 
+    Mistrust = 0;               // Недоверие к людям
+    Uncertainty = 0;            // Неуверенность в себе
+     // Акцентуация характера(Есть четыре вида)
+    Hyperthymic = 0;            // Гипертимная
+    Hysteroid = 0;              // Истероидная
+    Schizoid = 0;               // Шизоидная
+    EmotionallyLabile = 0;      // Эмоционально-лабильная
+    alert("И тут должны появиться результаты теста");
+    
+
+}
